63e582cf9f55b5c2f6e7c37cc2a0bea5e97be421
Pibo <piboman@yahoo.com>
2019-11-13 11:20:40 +0200

    add webotron script

---
+++ 01-webotron/webotron/webotron.py
@@ -0,0 +1,11 @@
+import boto3
+import sys
+
+session = boto3.Session(profile_name='pythonAutomation') 
+s3 = session.resource('s3')
+
+if __name__ == '__main__':
+	for bucket in s3.buckets.all():
+		print(bucket)
+
+
